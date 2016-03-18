f4dae78336659220dce8983bfa2fb020c7e60acb
Kyle Tucker <kyleatucker@gmail.com>
2016-03-18 14:35:04 -0400

    stub main script files

---
+++ src/Processor.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ft6k\ComposerDotenv;
+
+/**
+ * @author  Kyle Tucker <kyleatucker@gmail.com>
+ */
+class Processor
+{}
---
+++ src/ScriptHandler.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ft6k\ComposerDotenv;
+
+use Composer\Script\Event;
+
+/**
+ * Script handler for processing dotenv parameters.
+ *
+ * @author  Kyle Tucker <kyleatucker@gmail.com>
+ */
+class ScriptHandler
+{
+    public static function buildParameters(Event $event)
+    {}
+}
