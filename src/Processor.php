46c84c788a72994fdeeea1fe7a9ea55ff89d5a5d
Kyle Tucker <kyleatucker@gmail.com>
2016-03-21 11:51:58 -0400

    set up config processor

--- src/Processor.php
+++ src/Processor.php
@@ -3,6 +3,7 @@
 namespace Ft6k\ComposerDotenv;
 
 use Composer\IO\IOInterface;
+use Ft6k\ComposerDotenv\Exception\InvalidArgumentException;
 
 /**
  * @author  Kyle Tucker <kyleatucker@gmail.com>
@@ -11,6 +12,12 @@ class Processor
 {
     /** @var  IOInterface */
     protected $io;
+
+    /** @var  string  Full path to the ignored dotenv file. */
+    protected $file;
+
+    /** @var  string  Full path to the distributed dotenv template. */
+    protected $distFile;
 
     /**
      * Processor constructor.
@@ -30,7 +37,30 @@ class Processor
      * @param  array  $config
      */
     protected function processConfig(array $config)
-    {}
+    {
+        // Make sure keys exist first
+        if (!isset($config['file'])) {
+            throw new InvalidArgumentException('No dotenv file configured.');
+        } elseif (!isset($config['dist-file'])) {
+            throw new InvalidArgumentException('No dotenv distribution file configured.');
+        }
+
+        $cwd = getcwd();
+        $filePath = $cwd .'/'. $config['file'];
+        $distFilePath = $cwd .'/'. $config['dist-file'];
+
+        // Make sure the dist-file exists.
+        if (!is_file($distFilePath)) {
+            throw new InvalidArgumentException(sprintf(
+                'The specified dist-file "%s" does not exist.',
+                $distFilePath
+            ));
+        }
+
+        // Set paths
+        $this->file = $filePath;
+        $this->distFile = $distFilePath;
+    }
 
     /**
      * Run the dotenv processor.
