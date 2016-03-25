e4a3e2ee8b59aebf1f686cb5d3a8611e7cd27c85
Kyle Tucker <kyleatucker@gmail.com>
2016-03-25 15:08:52 -0400

    build out processor

--- src/Processor.php
+++ src/Processor.php
@@ -13,11 +13,14 @@ class Processor
     /** @var  IOInterface */
     protected $io;
 
+    /** @var  array  The processor configuration. */
+    protected $config = [];
+
     /** @var  string  Full path to the ignored dotenv file. */
-    protected $file;
+    protected $filePath;
 
     /** @var  string  Full path to the distributed dotenv template. */
-    protected $distFile;
+    protected $distFilePath;
 
     /**
      * Processor constructor.
@@ -28,7 +31,7 @@ class Processor
     public function __construct(IOInterface $io, array $config)
     {
         $this->io = $io;
-        $this->processConfig($config);
+        $this->config = $config;
     }
 
     /**
@@ -58,8 +61,8 @@ class Processor
         }
 
         // Set paths
-        $this->file = $filePath;
-        $this->distFile = $distFilePath;
+        $this->filePath = $filePath;
+        $this->distFilePath = $distFilePath;
     }
 
     /**
@@ -69,6 +72,79 @@ class Processor
      */
     public function run()
     {
+        $this->processConfig($this->config);
+
+        $distLoader = new ReadOnlyLoader($this->distFilePath);
+        $distParams = $distLoader->load();
+        
+        if (is_file($this->filePath)) {
+            $loader = new ReadOnlyLoader($this->filePath);
+            $params = $loader->load();
+        } else {
+            $params = [];
+        }
+
+        if (count($params) > 0 && !$this->config['keep-outdated']) {
+            // If keep-outdated isn't explicitly set true, remove any keys
+            // that don't exist in $distFile
+            $outdated = array_diff(array_keys($params), array_keys($distParams));
+            foreach ($outdated as $key) {
+                unset($params[$key]);
+            }
+        }
+
+        foreach ($distParams as $key => $value) {
+            if (!array_key_exists($key, $params)) {
+                if (!$this->io->isInteractive()) {
+                    // If interactive, prompt for a value for the new key
+                    $params[$key] = $this->io->ask(sprintf('<question>%s</question> (<comment>%s</comment>): ', $key, $value), $value);
+                } else {
+                    // Otherwise just use the dist value
+                    $params[$key] = $value;
+                }
+            }
+        }
+
+        // Write to $file.
+        return $this->writeOutput($params);
+    }
+
+    /**
+     * Write the merged parameters to the configured file.
+     *
+     * @param   array  $params
+     * @return  int
+     */
+    protected function writeOutput(array $params)
+    {
+        $lines = [];
+        foreach ($params as $key => $value) {
+            $lines[] = $this->writeLine($key, $value);
+        }
+
+        if (!file_put_contents($this->filePath, implode("\n", $lines), LOCK_EX)) {
+            $this->io->write(sprintf('<error>The file "%s" could not be written.</error>', $this->config['file']));
+
+            return 1;
+        }
+
+        $this->io->write(sprintf('<info>File "%s" written successfully.</info>', $this->config['file']));
+
         return 0;
+    }
+
+    /**
+     * Output a line.
+     * @param $key
+     * @param string $value
+     */
+    protected function writeLine($key, $value = '')
+    {
+        // Quote if necessary
+        if (preg_match('/\s+/', $value) === 1) {
+            $value = "'". preg_replace("/(?<!\\\)'/", "\'", $value) ."'";
+        }
+        
+        return sprintf('%s=%s', $key, $value);
     }
 }
